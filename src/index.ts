501dcc33e0caca4bbbabe8df916aa2954d26caef
André Abadesso <andre.abadesso@gmail.com>
2021-04-06 14:43:07 -0300

    refactor: using bi-directional callbacks on state machine instead of an activity

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import {
   Machine,
   assign,
   interpret,
+  send,
 } from 'xstate';
 import { syncToLatestBlockGen } from './utils';
 import {
@@ -20,33 +21,6 @@ import {
 } from './types';
 
 const WEBSOCKET_SERVER = process.env.WEBSOCKET_SERVER;
-
-const syncToLatestBlock = () => {
-  console.log('Started the syncToLatestBlock activity.');
-  const iterator = syncToLatestBlockGen();
-
-  const asyncCall: () => void = async () => {
-    for (;;) {
-      const block = await iterator.next();
-      if (block.done) {
-        break;
-      }
-
-      console.log('Downloaded block: ', block.value);
-    }
-
-    return;
-  };
-
-  asyncCall();
-
-  // This will stop the activity by force-return the iterator
-  return () => {
-    iterator.return();
-
-    return;
-  }
-};
 
 // TODO: We need to type the Event
 export const syncMachine = Machine<SyncContext, SyncSchema, any>({
@@ -60,29 +34,78 @@ export const syncMachine = Machine<SyncContext, SyncSchema, any>({
       always: [
         { target: 'syncing', cond: 'hasMoreBlocks' },
       ],
-      on: {
-        NEW_BLOCK: 'syncing'
-      }
+      on: { NEW_BLOCK: 'syncing' },
     },
     syncing: {
-      entry: ['resetMoreBlocks'],
-      activities: ['syncToLatestBlock'],
+      invoke: {
+        id: 'syncToLatestBlock',
+        src: (_context, _event) => (callback, onReceive) => {
+          const iterator = syncToLatestBlockGen();
+          const asyncCall: () => void = async () => {
+            for (;;) {
+              const block = await iterator.next();
+
+              const { value, done } = block;
+              if (done) {
+                console.log('Done!', value)
+                break;
+              }
+
+              if (value && !value.success) {
+                console.log('Erroed!', value.message);
+                callback('ERROR');
+                return;
+              }
+
+              console.log('Downloaded block: ', value);
+            }
+
+            return;
+          };
+
+          asyncCall();
+
+          onReceive((e) => {
+            if (e.type === 'STOP') {
+              console.log('Received STOP on onReceive.');
+              // This will migrate to IDLE and STOP
+              // the iterator
+              callback('DONE');
+            }
+          });
+
+          return () => {
+            console.log('Stopping the iterator.');
+            iterator.return();
+
+            return;
+          };
+        },
+      },
       on: {
         NEW_BLOCK: {
           actions: ['setMoreBlocks'],
-          target: 'idle',
         },
-        STOP: 'idle',
-      }
+        STOP: {
+          actions: send('STOP', {
+            to: 'syncToLatestBlock',
+          }),
+        },
+        DONE: 'idle',
+        ERROR: 'failure',
+      },
+      entry: [
+        'resetMoreBlocks',
+        send('START', {
+          to: 'syncToLatestBlock',
+        }),
+      ],
     },
     failure: {
       type: 'final',
     }
   }
 }, {
-  activities: {
-    syncToLatestBlock: () => syncToLatestBlock()
-  },
   guards: {
     hasMoreBlocks: (ctx) => ctx.hasMoreBlocks,
   },
@@ -117,20 +140,24 @@ machine.onTransition(state => {
 
 const handleMessage = (payload: any) => {
   if (payload.type === 'utf8') {
-    const message = JSON.parse(payload.utf8Data);
+    try {
+      const message = JSON.parse(payload.utf8Data);
 
-    switch(message.type) {
-      case 'dashboard:metrics':
-        break;
-      case 'network:new_tx_accepted':
-        if (!message.is_block) return;
-        if (message.is_voided) return;
-        console.log('New block');
-        machine.send({
-          type: 'NEW_BLOCK',
-          message,
-        })
-        break;
+      switch(message.type) {
+        case 'dashboard:metrics':
+          break;
+        case 'network:new_tx_accepted':
+          if (!message.is_block) return;
+          if (message.is_voided) return;
+
+          machine.send({
+            type: 'NEW_BLOCK',
+            message,
+          })
+          break;
+      }
+    } catch(e) {
+      console.log('E: ', e);
     }
   }
 };
