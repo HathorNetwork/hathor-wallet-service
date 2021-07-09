6bc142f278a56ce3f8dc3a94bc71064164e4521c
Luis Helder <luislhl@gmail.com>
2021-07-09 13:50:50 -0300

    refactor: Make sure the script fails if a command fails

--- scripts/build-and-push-docker.sh
+++ scripts/build-and-push-docker.sh
@@ -1,3 +1,5 @@
+set -e
+set -o pipefail
 
 if [ -z "$AWS_ACCOUNT_ID" ]; then
     echo "Please export a AWS_ACCOUNT_ID env var before running this";
@@ -11,7 +13,9 @@ if [ -z "$DOCKER_IMAGE_TAG" ]; then
 fi;
 
 echo $DOCKER_IMAGE_TAG;
+
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com;
+
 docker build -t $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/hathor-wallet-service-sync-daemon:$DOCKER_IMAGE_TAG .;
+
 docker push $AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/hathor-wallet-service-sync-daemon:$DOCKER_IMAGE_TAG;
-unset DOCKER_IMAGE_TAG
