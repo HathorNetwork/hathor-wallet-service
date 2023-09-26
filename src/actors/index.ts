8c8c080eda2cb2e7cdf1a947c0c8cab8b65d3d17
André Abadesso <andre.abadesso@gmail.com>
2023-09-26 11:44:29 -0300

    feat: added websocket actor

--- src/actors/index.ts
+++ src/actors/index.ts
@@ -1,8 +1,2 @@
-/**
- * Copyright (c) Hathor Labs and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-export const WebSocketActor = () => {};
+export { default as WebSocketActor } from './WebSocketActor';
+export * from './WebSocketActor';
