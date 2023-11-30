9a3f62928e53ddd00aed135efbdb888634feb6c7
André Abadesso <andre.abadesso@gmail.com>
2023-11-30 11:48:30 -0300

    chore: updated daemon package with reliable-integrations

--- packages/daemon/src/logger.ts
+++ packages/daemon/src/logger.ts
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-<<<<<<< HEAD
 import { createLogger, format, transports } from 'winston';
 import getConfig from './config';
 
@@ -23,47 +22,3 @@ export default createLogger({
     new transports.Console(),
   ],
 });
-
-=======
-
-import util from 'util';
-import * as winston from 'winston';
-
-const CONSOLE_LEVEL = process.env.CONSOLE_LEVEL || 'info';
-
-const myFormat = winston.format.printf(
-  ({ level, message, ...args }) => {
-    let argsStr = '';
-
-    if (Object.keys(args).length > 0) {
-      // Adapted from https://github.com/winstonjs/logform/blob/master/pretty-print.js
-      const stripped = { ...args };
-
-      argsStr = util.inspect(stripped, {
-        compact: true,
-        breakLength: Infinity,
-      });
-    }
-
-    return `${Date.now()} [wallet-service-daemon] ${level}: ${message} ${argsStr}`;
-  },
-);
-
-const transports = [
-  new winston.transports.Console({
-    format: winston.format.combine(winston.format.colorize(), myFormat),
-    level: CONSOLE_LEVEL,
-  }),
-];
-
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.errors({ stack: true }),
-  ),
-  defaultMeta: { service: 'wallet-service-daemon' },
-  transports,
-});
-
-export default logger;
->>>>>>> 554cebd (feat: added sync machine to the project with mocked services)
