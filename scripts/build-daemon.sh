594e172209e5e1ab4a94e88f8f13e366450d44e5
André Abadesso <andre.abadesso@gmail.com>
2024-02-23 11:18:24 -0300

    chore: added comments on build-daemon script

--- scripts/build-daemon.sh
+++ scripts/build-daemon.sh
@@ -6,15 +6,20 @@ if [ -z "$ACCOUNT_ID" ]; then
     exit 1;
 fi
 
+# Fetch the image tag from the temp file, this should be filled if the stage
+# is not `dev`
 DOCKER_IMAGE_TAG=$(cat /tmp/docker_image_tag 2>/dev/null || echo "")
 
 if [ -z "$DOCKER_IMAGE_TAG" ]; then
     commit=`git rev-parse HEAD`;
     timestamp=`date +%s`;
+    # Default to the dev image tag
     DOCKER_IMAGE_TAG="dev-$commit-$timestamp";
 fi;
 
 echo $DOCKER_IMAGE_TAG;
+# Store the updated image tag in the tmp file so the upload stage is able to use
+# it
 echo $DOCKER_IMAGE_TAG > /tmp/docker_image_tag;
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com;
