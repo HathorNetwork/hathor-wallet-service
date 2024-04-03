83eae6e505b49bd711d18844809c02c04c34d6de
André Abadesso <andre.abadesso@gmail.com>
2024-04-03 13:22:37 -0300

    chore: added hathor header

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,4 +1,11 @@
 /**
+ * Copyright (c) Hathor Labs and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
  * Alerts should follow the on-call guide for alerting, see
  * https://github.com/HathorNetwork/ops-tools/blob/master/docs/on-call/guide.md#alert-severitypriority
  */
