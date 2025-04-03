d86890507bd2b00c0a86058711682b4ab2313bc2
André Carneiro <andreluizmrcarneiro@gmail.com>
2025-04-03 16:13:36 -0300

    feat: improve error logs and typing

--- packages/wallet-service/src/api/middlewares/errorHandler.ts
+++ packages/wallet-service/src/api/middlewares/errorHandler.ts
@@ -5,23 +5,29 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import middy from '@middy/core'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import createDefaultLogger from "@src/logger"
 
 const logger = createDefaultLogger();
 
-const errorHandler = () => {
-   const onError = async (request) => {
-    logger.error(request.error);
+const defaultResponse: APIGatewayProxyResult = { statusCode: 500, body: "Internal Server Error" };
 
+const errorHandler = (): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
+   const onError: middy.MiddlewareFn<APIGatewayProxyEvent, APIGatewayProxyResult> = async (request)  => {
+    logger.error(`[${request.context.functionName}] Unhandled error on ${request.event.path}: ${request.error}`);
+
+    // Initialize response with default values if it hasn't been done yet.
+    request.response = request.response ?? {...defaultResponse};
+    // Force the status code to 500 since this is an unhandled error
+    request.response.statusCode = 500;
+
+    // In production, we do not want to expose the error message to the user.
     if (process.env.NODE_ENV === 'production') {
-      request.response = request.response ?? {}
-      request.response.statusCode = 500;
       request.response.body = "Internal Server Error";
       return request.response;
     }
 
-    request.response = request.response ?? {}
-    request.response.statusCode = 500;
     request.response.body = request.error?.message || String(request.error);
     return request.response;
   }
