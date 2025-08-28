8ab7033736c6107de681abe1df63c34f3e03aae8
Tulio Miranda <tulio.mir@gmail.com>
2025-08-28 19:28:05 -0300

    feat: build-daemon script no longer needs aws

--- scripts/build-daemon.sh
+++ scripts/build-daemon.sh
@@ -1,8 +1,13 @@
 set -e
 set -o pipefail
 
+# ACCOUNT_ID is a mandatory env var because the script was built to be used in AWS environments,
+# however it's no longer mandatory to publish it there. So we're keeping this variable temporarily and
+# implementing a specific behavior in case it's set to "NONE".
+
 if [ -z "$ACCOUNT_ID" ]; then
     echo "Please export a ACCOUNT_ID env var before running this";
+    echo "Set it to NONE if you don't want to publish to AWS ECR";
     exit 1;
 fi
 
@@ -22,10 +27,22 @@ echo $DOCKER_IMAGE_TAG;
 # it
 echo $DOCKER_IMAGE_TAG > /tmp/docker_image_tag;
 
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com;
+if [ "$ACCOUNT_ID" = "NONE" ]; then
+  echo "== Skipping AWS ECR login since ACCOUNT_ID is set to NONE";
+  FINAL_TAG="hathornetwork/hathor-wallet-service-sync-daemon:latest";
+else
+  aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com;
+  FINAL_TAG="$ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/hathor-wallet-service-sync-daemon:$DOCKER_IMAGE_TAG"
+fi;
+
+# Copying the correct .dockerignore file
+cp packages/daemon/.dockerignore .dockerignore;
 
 # Fetching the daemon Dockerfile to build
 docker build \
-  -t $ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/hathor-wallet-service-sync-daemon:$DOCKER_IMAGE_TAG\
+  -t $FINAL_TAG\
   -f packages/daemon/Dockerfile\
   .;
+
+# Removing the copied .dockerignore file to avoid confusion with other builds in this monorepo
+rm .dockerignore
