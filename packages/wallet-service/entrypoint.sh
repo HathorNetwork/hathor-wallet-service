2bbd9f257388495ae8b4f9cd2ddfac6dcbd0d07e
tuliomir <tulio.mir@gmail.com>
2025-09-09 12:34:24 -0300

    refactor: mock fixtures to the tests folder

--- packages/wallet-service/entrypoint.sh
+++ packages/wallet-service/entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+# When MOCK_AWS is set to true, copy the mocked AWS credentials fixtures to .aws the working directory
+if [ "$MOCK_AWS" = "true" ]; then
+  cp -r tests/fixtures/aws ./.aws
+fi
+
 yarn serverless offline start --host 0.0.0.0 --httpPort 3000
