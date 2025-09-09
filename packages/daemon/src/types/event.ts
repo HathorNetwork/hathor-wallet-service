9be50a0f6e9d78a35de0860cec8d0616a5b6109d
Andre Carneiro <andreluizmrcarneiro@gmail.com>
2025-09-08 23:12:27 -0300

    chore: add decoded alternative

--- packages/daemon/src/types/event.ts
+++ packages/daemon/src/types/event.ts
@@ -103,9 +103,11 @@ export const EventTxOutputSchema = z.object({
       type: z.string(),
       address: z.string(),
       timelock: z.number().nullable(),
-      token_data: z.number().nullish(),
     }).passthrough().nullable(),
-    z.object({}).strict()
+    z.object({}).strict(),
+    z.object({
+      token_data: z.number().nullish(),
+    }).strict()
   ]),
 });
 export type EventTxOutput = z.infer<typeof EventTxOutputSchema>;
