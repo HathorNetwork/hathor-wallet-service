51a02cd52d716f6afb27288685c3e09e7b9d5522
André Abadesso <andre.abadesso@gmail.com>
2025-12-12 19:01:18 -0300

    chore(daemon): added first block migration

---
+++ db/migrations/20251212000000-add-first-block-to-token-creation.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('token_creation', 'first_block', {
+      type: Sequelize.STRING(64),
+      allowNull: true,
+      comment: 'First block hash that confirmed the nano contract execution that created this token',
+    });
+
+    // Add index on first_block for efficient lookups during reorg
+    await queryInterface.addIndex('token_creation', ['first_block'], {
+      name: 'token_creation_first_block_idx',
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeIndex('token_creation', 'token_creation_first_block_idx');
+    await queryInterface.removeColumn('token_creation', 'first_block');
+  },
+};
